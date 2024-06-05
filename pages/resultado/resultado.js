4eb771a3f2ddcdb8e7b21dffca7b87284ee9b976
renato pacheco <renato.almeida@eaportal.org>
2024-06-05 20:35:33 +0000

    Adicionada a função de inserir o resultado na tela

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -2,11 +2,40 @@ import { trocarTema, verificarTema } from "../../helpers/tema-helper.js"
 
 
 const botaoTema = document.querySelector(".tema button")
-
 const body = document.querySelector("body")
+const assunto = localStorage.getItem("assunto")
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
 
 verificarTema(body, botaoTema)
+
+function alterarAssunto(){
+    const divIcon = document.querySelector(".assunto_icon")
+    const iconeImg = document.querySelector(".assunto_icon img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcon.classList.add(assunto.toLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
+    iconeImg.setAttribute("alt", `icone de ${assunto}`)
+    assuntoTitulo.innerHTML = assunto
+}
+
+alterarAssunto()
+
+function inserirResultado (){
+    const sectionPontuacao = document.querySelector(".pontuacao")
+    const divAssunto = document.querySelector(".assunto")
+    const pontos = localStorage.getItem("pontos")
+
+    sectionPontuacao.innerHTML = `
+       ${divAssunto.outerHTML}
+
+        <strong>${pontos}</strong>
+
+        <p>de 10</p>
+    `
+}
+
+inserirResultado()
